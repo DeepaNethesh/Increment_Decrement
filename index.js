4a1f3ac823a028231c423363ebd75e16a4bb81a7
DeepaNethesh <deepanethesh2011@gmail.com>
2021-03-01 19:43:48 +1100

    add logic

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-// const button1 = document.querySelector('#AddButton')
-// const text = document.querySelector('#textName')
-// console.log(text)
-// button1.addEventListener('click', () => {
-//     text.value++
-// })
+const button1 = document.querySelector('#AddButton')
+const button2 = document.querySelector('#SubtractButton')
+const text = document.querySelector('#textName')
+console.log(text)
+button1.addEventListener('click', () => {
+    text.value++
+})
+button2.addEventListener('click',() => {
+    text.value -= 1
+})
